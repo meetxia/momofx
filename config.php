e8897c2d8c4d2cf6669924edc06377d3b925e5d1
meetxia <38974054+meetxia@users.noreply.github.com>
2024-12-22 13:45:25 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+//MySQL database host
+const DB_HOST = 'localhost';
+//Database username
+const DB_USER = '123456';
+//Database user password
+const DB_PASSWD = '123456';
+//Database name
+const DB_NAME = '123456';
+//Database Table Prefix
+const DB_PREFIX = 'emlog_';
+//Auth key
+const AUTH_KEY = 'PcT526sMK&Y$ai0#gN9$Pv5KXvSFTrgSef6c1824c2c4d2e2dd0ed5a2ca40bef3';
+//Cookie name
+const AUTH_COOKIE_NAME = 'EM_AUTHCOOKIE_b7boapRQUz0PYelWn0Lea0fTrQMShpyH';
